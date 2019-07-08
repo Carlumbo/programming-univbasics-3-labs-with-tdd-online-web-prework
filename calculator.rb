083e10919e16eb9eb31a09e70f05a28ad7332be3
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-08 20:30:38 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,5 @@ first_number = 1 + 1
 second_number = first_number + 5
 sum = first_number + second_number
 difference = first_number - second_number
+product = first_number * second_number
+blurp = first_number / second_number
