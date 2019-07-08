c254baf51ce89465aa39f96a8fe06efa926a83c6
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-08 20:30:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,4 +4,4 @@ second_number = first_number + 5
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
-blurp = first_number / second_number
+quotient = first_number / second_number
